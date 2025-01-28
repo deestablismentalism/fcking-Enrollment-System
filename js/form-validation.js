5adf505e7bf7a48fb9ae1212538bb8c007bdac33
deestablismentalism <davidiyad6@gmail.com>
2025-01-28 22:41:13 +0800

    change textbox to combo box

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded',function(){
     const form = document.getElementById("enrollment-form");
     const startYear = document.getElementById("start-year");
     const endYear = document.getElementById("end-year");
-    const gradesTbe = document.getElementById("grades-tbe");
-    const lastGrade = document.getElementById("last-grade");
     const lastYear = document.getElementById("last-year");
     const errorMsg = document.getElementById("error-messages");
 
@@ -11,41 +9,44 @@ document.addEventListener('DOMContentLoaded',function(){
     const year = new Date().getFullYear();
     startYear.value = year;
     endYear.value = year + 1;
-   
+    const startYearVal = parseInt(startYear.value);
+   //ensure that the start year is always higher or equal to the current year
+   function validateStartYear() {
+        if(startYearVal < year) {
+            startYear.style.border = "1px solid red";
+        }
+   }
     //ensure that the end year is always greater than the start year 
     function validateAcademicYear(){
-        const startYearVal = parseInt(startYear.value);
         const endYearVal = parseInt(endYear.value);
-        
-        if (endYearVal < startYearVal) {
+        if (endYearVal < startYearVal || endYearVal == startYearVal) {
             endYear.style.border = "1px solid red";
-        }
-        else if (startYearVal < year) {
-            startYear.style.border = "1px solid red";
         }
         else {
             endYear.style.border = "1px solid #616161";
         }
     }
       //check if the last school  year finished is not greater than the current year
-            lastYear.addEventListener('change', function(){
+    function validateYearFinished(){
       
-                const lastYearVal = parseInt(lastYear.value);
+        const lastYearVal = parseInt(lastYear.value);
         
-                if (lastYearVal > year) {
-                    lastYear.style.border = "1px solid red";
-                }
-                else {
-                    lastYear.style.border = "1px solid #616161";
-                }
-            });
+        if (lastYearVal < year) {
+            lastYear.style.border = "1px solid red";
+        }
+        else {
+            lastYear.style.border = "1px solid #616161";
+        }
+    }
+        startYear.addEventListener('change', validateStartYear);
+        endYear.addEventListener('change',validateAcademicYear);
+        lastYear.addEventListener('change', validateYearFinished);
 
-            form.addEventListener('submit', function(e){
-                e.preventDefault();
-                
-                validateAcademicYear();
-            });
+        form.addEventListener('submit', function(e){
+            e.preventDefault();
+            validateYearFinished();
+            validateAcademicYear();
+        });
 
-    const lastGradeVal = parseInt(lastGrade.value);
 });    
  
