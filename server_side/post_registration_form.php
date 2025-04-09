c5d21725a84439622ae60a4a149f8077b3e7fc1e
Lovelyy <lovelycruz.1317@gmail.com>
2025-04-09 21:35:53 +0800

    Added Phone Verification and Regex expressions

--- server_side/post_registration_form.php
+++ server_side/post_registration_form.php
@@ -1,26 +1,18 @@
 <?php
     require_once 'backend_registration.php';
 
+    header("Content-Type: application/json");
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $registration = new Registration();
         
-        // PERSONAL INFORMATION
-        $First_Name = $_POST['Guardian-First-Name'];
-        $Last_Name = $_POST['Guardian-Last-Name'];
-        $Middle_Name = $_POST['Guardian-Middle-Name'];
-        $Contact_Number = $_POST['Contact-Number'];
+        $result = $registration->register(
+            $_POST['Guardian-First-Name'],
+            $_POST['Guardian-Last-Name'],
+            $_POST['Guardian-Middle-Name'],
+            $_POST['Contact-Number']
+        );
         
-        // Register the user
-        $registration->register($First_Name, $Last_Name, $Middle_Name, $Contact_Number);
+        echo json_encode($result);
     }
-?>
-
-<?php
-header("Content-Type: application/json");
-
-$response = [
-    "message" => "Registration successful!"
-];
-
-echo json_encode($response);
 ?>
