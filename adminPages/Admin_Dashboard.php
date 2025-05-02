e30f12a7b19c7c3f4f2b71635fbe709ccb6dde95
Lovelyy <lovelycruz.1317@gmail.com>
2025-05-03 01:16:22 +0800

    done pinapaedit ni sir

--- adminPages/Admin_Dashboard.php
+++ adminPages/Admin_Dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Dashboard</title>
     <link rel="stylesheet" href="../css/admin_dashboard.css"> 
     <?php
         include './admin_base_designs.php'; 
@@ -15,7 +15,7 @@
                 <!--Enrolled-->
                 <div class="card card-1">
                     <div class="card-image border-50">
-                        <img src="../imgs/student.svg" alt="">
+                        <img src="../imgs/enrolled.png" alt="">
                         <p class="card-text">Enrolled</p>   
                     </div>
                     <div class="number-div">
@@ -27,36 +27,36 @@
                 <!--Pending Enrollees-->
                 <div class="card card-2">
                     <div class="card-image border-50">
-                        <img src="../imgs/student.svg" alt="">
+                        <img src="../imgs/pending.png" alt="">
                         <p class="card-text">Pending Enrollees</p>   
                     </div>
                     <div class="number-div">
                         <p class="number">
-                            1300
+                            450
                         </p>
                     </div>
                 </div>
                 <!--To Follow Up-->
                 <div class="card card-3">
                     <div class="card-image border-50">
-                        <img src="../imgs/student.svg" alt="">
-                        <p class="card-text">To Follow up</p>   
+                        <img src="../imgs/follow.png" alt="">
+                        <p class="card-text">To Follow-up</p>   
                     </div>
                     <div class="number-div">
                         <p class="number">
-                            1300
+                            213
                         </p>
                     </div>
                 </div>
                 <!--Early Registration-->
                 <div class="card card-4">
                     <div class="card-image border-50">
-                        <img src="../imgs/student.svg" alt="">
+                        <img src="../imgs/Early.png" alt="">
                         <p class="card-text">Early Registration</p>   
                     </div>
                     <div class="number-div">
                         <p class="number">
-                            1300
+                            59
                         </p>
                     </div>
                 </div>
@@ -73,34 +73,34 @@
                             <th>Contact Number</th>
                         </tr>
                         <tr>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
+                            <td># 33595868798</td>
+                            <td>Lovely Jane Dela Cruz</td>
+                            <td>4</td>
+                            <td># 0991209977</td>
                         </tr>
                         <tr>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
+                            <td># 33595868798</td>
+                            <td>Kenneth Jeffrey Alojado</td>
+                            <td>3</td>
+                            <td># 0991209977</td>
                         </tr>
                         <tr>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
+                            <td># 33859350393</td>
+                            <td>Nemesio Benedict Llorin</td>
+                            <td>5</td>
+                            <td># 0991209977</td>
                         </tr>
                         <tr>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
+                            <td># 332113384993</td>
+                            <td>Jearard David</td>
+                            <td>6</td>
+                            <td># 093120808</td>
                         </tr>
                         <tr>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
-                            <td>Hi</td>
+                            <td># 33859350393</td>
+                            <td>Chong Tae</td>
+                            <td>6</td>
+                            <td># 0991209977</td>
                         </tr>
                     </table>
                 </div>
