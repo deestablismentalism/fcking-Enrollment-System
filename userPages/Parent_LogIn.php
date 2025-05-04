8832c61be9cc57aa35ba881cd7db3068d781b0f8
Lovelyy <lovelycruz.1317@gmail.com>
2025-05-04 15:09:47 +0800

    done parent log in , choose account

--- userPages/Parent_LogIn.php
+++ userPages/Parent_LogIn.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Parent LogIn </title>
+    <link rel="stylesheet" href="../css/fonts.css">
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/Parent_LogIn.css">
 </head>
@@ -12,34 +13,49 @@
 <body>
     <div class="main">
 
-        <div class="left-div">
+        <div class="left-container">
             <div class="header">
-                <h2>Lucena South II Elementary School</h2>
+                <h2>Lucena  South  II  Elementary  School</h2>
             </div>
             <div id="logo-container">
-                    <img src="../imgs/mother.jpg" 
-                    alt="Parent-Image" id="Parent-Image">
-
-                    <table>
-                        <tr>
-                        </tr>
-                    </table>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img src="../imgs/mother.jpg" alt="Parent-Image" id="Parent-Image">
+                        </div>
+                        <div class="flip-card-back">
+                            <img src="../imgs/glass.jpg" alt="back-image" id="back-image">
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div class="mother-name">
+                <h2>Irene Maria s. Villamayor</h2>
+                <hr>        
+                <h1>Parent Account</h1>
+            </div>
+
+            <p>
+                <a href="../userPages/Student_Dashboard.php" class="Enroll-New-Link">
+                   Mag-enroll
+                </a>
+                <span style="color: white;">ng Panibago</span>
+            </p>
         </div>
 
-        <div class="right-div">
-            <div>
-                <form name=f1 method=post action=test.php>
-                    <select name=s1>
-                        <option value=1>Option 1</option>
-                        <option value=2>Option 2</option>
-                        <option value=3>Option 3</option>
-                        <option value=4>Option 4</option>
-                        <option value=5>Option 5</option>
-                    </select>
-                    
-                </form>                    
+        <div class="right-container">
+                <h3> Pumili  ng  Account </h3>
+                <h4> Mga Account ng Estudyante: </h4>
+
+
+            <div class="btn-group" style="width:100%">
+                <button style="width:47%">Kenjie Villamayor</button>
+                <button style="width:47%">Jane Villamayor</button>
+                <button style="width:47%">Jearard Villamayor</button>
+                <button style="width:47%">Ben Villamayor</button>
             </div>
+        
+            
         </div>
     </div>
         
