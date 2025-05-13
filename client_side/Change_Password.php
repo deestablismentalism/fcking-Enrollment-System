5df38757a65fe34602044558c4bf62543a31316f
Lovelyy <lovelycruz.1317@gmail.com>
2025-05-14 00:08:33 +0800

    css update done to change password page

--- client_side/Change_Password.php
+++ client_side/Change_Password.php
@@ -3,24 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Change Password</title>
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/Change_Password.css">
+    <link rel="stylesheet" href="../css/fonts.css">
     <script src="../js/change-password.js"></script>
 </head>
 <body>
-    <div class="main-container">
-        <div class="header">
-            <h2>CHANGE PASSWORD</h2>
-        </div>
-        <div class="input-boxes">
-            <form id="change-password-form" action="../server_side/post_change_password.php" method="post"><br>
-                <input type="password" id="old-password" name="old-password" placeholder="Old Password" required><br>
-                <input type="password" id="new-password" name="new-password" placeholder="New Password" required><br>   
-                <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm New Password" required><br>
-                <button type="submit" class="submit">Change Password</button>
-            </form>
-        </div>
+    <div class="bg-image">
+        <div class="blurred-background"></div>
+            <div class="form" class="input-boxes">
+            <h2 style="text-align: center;">Change Password</h2>
+                <form id="change-password-form" class="overlay-form" action="../server_side/post_change_password.php" method="post"><br>
+                    <input type="password" id="old-password" name="old-password" placeholder="Old Password" required><br>
+                    <input type="password" id="new-password" name="new-password" placeholder="New Password" required><br>   
+                    <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm New Password" required><br>
+                    <button type="submit" class="submit" style="center">Change Password</button>
+                </form>
+            </div>
     </div>
 </body>
 </html>
