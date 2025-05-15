f602a194d36329094d694117f30da78e27a601ba
Lovelyy <lovelycruz.1317@gmail.com>
2025-05-16 01:06:53 +0800

    done responsiveness of user login and registration, extrawide screen is still on progress

--- client_side/login_form.php
+++ client_side/login_form.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <meta ssrset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
     <link rel="stylesheet" href="../css/reset.css">
